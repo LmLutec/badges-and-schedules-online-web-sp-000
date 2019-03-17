06fee4e3603ef89f01e4b9759dedd6b79d321da3
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-17 22:12:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ end
 
 def printer(attendees)
    batch_badge_creator(attendees).each_with_index.map {|attendees, index| 
-      puts "Hello, my name is #{attendees}. You'll be assigned to room #{index + 1}!"}
+      puts "Hello, my name is #{attendees}."} # You'll be assigned to room #{index + 1}!"}
    
 end 
